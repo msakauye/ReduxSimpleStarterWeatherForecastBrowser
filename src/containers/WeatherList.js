d7e7c0ac21f4b288404d61c77d82016135249985
Mark Sakauye <msakauye@uthsc.edu>
2018-02-15 22:18:22 -0500

    add map view to table

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Graph from '../components/Graph';
+import GoogleMap from '../components/GoogleMap';
 
 
 class WeatherList extends Component {
     renderWeather(cityData) {
+        console.log(cityData);
         const tempData = cityData.list.map(datum => datum.main.temp);
         const pressureData = cityData.list.map(datum => datum.main.pressure);
         const humidityData = cityData.list.map(datum => datum.main.humidity);
+        const { lon, lat } = cityData.city.coord;
 
         return (
             <tr key={cityData.city.id}>
-                <td>{cityData.city.name}</td>
+                <td>
+                    <GoogleMap lat={lat} lon={lon} />
+                </td>
                 <td>
                     <Graph data={tempData} color="red" unit="K" />
                 </td>
